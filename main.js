434b3b88ed5d45fcd9cd4fbddeac58371bc15eb5
Stevixent <chiadikavirtue@gmail.com>
2023-02-01 15:13:46 +0100

    ok

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let btnRef = document.querySelectorAll(".button-option");
 let popupRef = document.querySelector(".popup");
 let newgameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
-let msgRef = document.getElementById("message");
+let msgRef = document.getElementById(".message");
 //Winning Pattern Array
 let winningPattern = [
   [0, 1, 2],
